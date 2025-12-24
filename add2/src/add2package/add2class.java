c0902479886801d438be1083d7fb5a69455e58cc
arvpy <ariyavijay21@gmail.com>
2025-12-24 21:50:20 +0530

    testing

---
+++ add2/src/add2package/add2class.java
@@ -0,0 +1,31 @@
+package add2package;
+
+
+//import java.util.Scanner;
+
+public class add2class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* int x,y,sum;
+		Scanner m=new Scanner(System.in);
+		System.out.println("Enter two numbers");
+		x=m.nextInt();
+		y=m.nextInt();
+		sum=x+y;
+		System.out.println("Sum is "+sum);*/
+		
+			// initial counter variable
+			int i = 0;
+			do {
+			                System.out.println("Print statement");
+			             
+			       }
+			
+			while (++i <2 );
+			
+		}
+
+	}
+
+
